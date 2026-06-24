eec22b96254fdcbea74d5c6e9f0e1b723b7b191c
ShadeEdge <shadeedge431@gmail.com>
2026-06-24 22:36:33 +0300

    Slash command handler with DJs commander has been deleted

--- Slash Command handler/src/commands/ping.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-
-module.exports = {
-    data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
-
-   async run ({ interaction }) {
-    interaction.reply('Pong!')
-   }
-}
--- Slash Command handler/src/events/clientReady/consoleLog.js
+++
@@ -1,3 +0,0 @@
-module.exports = (client) => {
-    console.log(`My bot is online!`)
-} 
